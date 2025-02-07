2756e9fe36adebd17142c9415fca29d54f99b3b3
peter <zhangnianhao@ruc.edu.cn>
2025-02-07 22:12:19 +0800

    feat: implement dog creation and birthday celebration features in CapersRepository

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -5,14 +5,12 @@ import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Peter
 */
-public class Dog { // TODO
+public class Dog implements Serializable {
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
-
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CWD,".capers","dogs");
     /** Age of dog. */
     private int age;
     /** Breed of dog. */
@@ -39,7 +37,10 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        if (dogFile.exists()) {
+            return Utils.readObject(dogFile, Dog.class);
+        }
         return null;
     }
 
@@ -56,7 +57,8 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        Utils.writeObject(dogFile, this);
     }
 
     @Override
