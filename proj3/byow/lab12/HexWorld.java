a55e91b959ecd55c77633eba14c014e012a6c969
peter <zhangnianhao@ruc.edu.cn>
2025-04-07 21:29:18 +0800

    (done) lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -1,15 +1,56 @@
 package byow.lab12;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
-import java.util.Random;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    static final int WIDTH = 50;
+    static final int HEIGHT = 50;
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] tiles = new TETile[WIDTH][HEIGHT];
+        fillEmpty(tiles);
+        printHexagon(10, 10, 4, tiles, Tileset.FLOWER);
+
+        ter.renderFrame(tiles);
+    }
+
+    private static void fillEmpty(TETile[][] tiles) {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private static void printHexagon(int x, int y, int size, TETile[][] tiles, TETile tile) {
+        for (int layer = 0; layer < size; layer++) {
+            int layerY = y + layer;
+            if (layerY >= HEIGHT) {
+                break;
+            }
+            for (int lX = max(0, x - layer); lX < min(WIDTH, x + size + layer); lX++) {
+                tiles[lX][layerY] = tile;
+            }
+        }
+        for (int layer = 0; layer < size; layer++) {
+            int layerY = y + layer + size;
+            if (layerY >= HEIGHT) {
+                break;
+            }
+            for (int lX = max(0, x - (size - layer) + 1); lX < min(WIDTH, x + 2*size - layer - 1); lX++) {
+                tiles[lX][layerY] = tile;
+            }
+        }
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,22 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer: i just accomplished draw, i don't came up with the idea that make Unit test for them, what i do just using my eyes to exam it. where to place looks like a math trike.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
-What is the hexagon and what is the tesselation on the Project 3 side?**
+What is the hexagon and what is the tessellation on the Project 3 side?**
 
-Answer:
+Answer: tessellating the rooms, generate hallways to connect the rooms.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: generate a signal room,with walls and floor, use some methods to connect room by hallways with floor, think about ways to generate walls and avoid conflicts, add a win rule to a certain room.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: hallway are just 1 or 2 tiles and random length
